b5a19425fcb06975143cba6af481199de7159ff9
josers77j <josers77.1@gmail.com>
2026-03-18 22:39:23 -0600

    chore: prisma downgrade

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 
 import { PrismaModule } from './prisma/prisma.module';
+import { ProvidersModule } from './modules/providers/providers.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ProvidersModule, ProductsModule],
   controllers: [],
   providers: [],
 })
